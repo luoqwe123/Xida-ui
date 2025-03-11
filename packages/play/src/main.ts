7b5517898236d10b0c98f800001ed9198d92aece
风风 <qwe18173945756@163.com>
2025-03-11 21:01:52 +0800

    chore:配置了ci/cd

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -3,7 +3,7 @@ import './style.css'
 import App from './App.vue'
 import  Xida from "xida-ui"
 
-console.log( "main.ts",Xida)
+// console.log( "main.ts",Xida)
 let app = createApp(App)
 
 
--- packages/utils/install.ts
+++ packages/utils/install.ts
@@ -4,19 +4,21 @@ import { each } from "lodash-es";
 type SFCWithInstall<T> = T & Plugin;
 //安装一个插件
 export function makeInstaller(components: Plugin[]) {
-  const install = (app: App) =>
+  const install = (app: App,options?:any[]) => {
     each(components, (c) => {
-     console.log("c",c)
+      // console.log("c", c)
       app.use(c);
+     
     });
- 
+   
+  }
   return install;
 }
 // ast  转化为抽象语法树
 export const withInstall = <T>(component: T) => {
-  console.log("utlis WI",(component));
+  // console.log("utlis WI", (component));
   (component as SFCWithInstall<T>).install = (app: App) => {
-    console.log(1231231)
+
     const name = (component as any)?.name || "UnnamedComponent";
     app.component(name, component as SFCWithInstall<T>);
   };
